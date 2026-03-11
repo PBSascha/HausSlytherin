9b353540e3dce649ae05475dab1d0253a489bbac
PBSascha <pb-00-gebsas1@schulung-bbw.de>
2026-03-11 12:06:25 +0100

    Aufgabe 11

---
+++ HausSlytherin/Aufgabe11.cs
@@ -0,0 +1,7 @@
+//Wann verwendet man Exceptions?
+//Exceptions werden verwendet, um Fehler oder unerwartete Situationen in einem Programm zu 
+//behandeln. Sie ermöglichen es, den normalen Ablauf eines Programms zu unterbrechen und
+//stattdessen eine alternative Fehlerbehandlung durchzuführen.
+//Hier sind einige Situationen, in denen Exceptions verwendet werden sollten:
+//1. Fehlerhafte Benutzereingaben: Wenn ein Benutzer ungültige Daten eingibt, wie z.B. eine falsche Zahl oder einen ungültigen Dateipfad, können Exceptions verwendet werden, um diese Fehler zu erkennen und angemessen darauf zu reagieren.
+//2. Netzwerkfehler: Wenn ein Programm versucht, eine Verbindung zu einem Server herzustellen
